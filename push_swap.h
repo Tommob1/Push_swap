8905554ce7be774913a87cdc575ff21941215951
Brayden Tomlinson <humortronic@gmail.com>
2024-02-19 21:29:52 +1030

    Added changes to code

--- push_swap.h
+++ push_swap.h
@@ -11,12 +11,12 @@ typedef struct s_stack_node
     int                 nbr;
     int                 index;
     int                 push_cost;
-    int                 above_median;
-    int                 cheapest;
-
+    bool                above_median;
+    bool                cheapest;
+    struct s_stack_node *target_node;
     struct s_stack_node *next;
     struct s_stack_node *prev;
     
-}
+}   t_stack_node;
 
 #endif
