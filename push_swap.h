767b28e4338d25566edd7fd62aeabd7e0789fa43
Brayden Tomlinson <humortronic@gmail.com>
2024-02-19 21:17:54 +1030

    Added changes to code

---
+++ push_swap.h
@@ -0,0 +1,13 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdbool.h>
+# include <limits.h>
+//# include </libft/inc/libft.h>
+//# include <../libft/inc/ft_printf.h>
+
+typedef struct s_stack_node
+{
+}
+
+#endif
