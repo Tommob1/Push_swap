280b41a8b8a9ddd75da323068a4f844f3983ec61
Brayden Tomlinson <humortronic@gmail.com>
2024-02-20 12:16:47 +1030

    Added changes to code

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
-#include <unistd.h>
+#include "push_swap.h"
 
 int main(int argc, char **argv)
 {
+    t_stack_node    *a;
+    t_stack_node    *b;
 
-
+    a = NULL;
+    b = NULL;
+    if (argc == 1 || (argc == 2 && !argv[1][0]))
+        return (1);
     
-    return 0;
+    return (0);
 }
