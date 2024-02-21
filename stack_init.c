7a2ebad2fc3fc50e446dd4905158359d8d4190b7
Brayden Tomlinson <humortronic@gmail.com>
2024-02-21 14:27:03 +1030

    Added changes

--- stack_init.c
+++ stack_init.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:02:11 by btomlins          #+#    #+#             */
-/*   Updated: 2024/02/21 14:11:56 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/02/21 14:26:48 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,9 @@ static long ft_atol(const char *s)
     while (ft_isdigit(*s))
         result = result * 10 + (*s++ - '0');
     return (result * sign);
+}
+
+static void append_node(t_stack_node **stack, int n)
+{
+    
 }
