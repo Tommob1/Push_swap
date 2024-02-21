f23519a6fc8d0b15eec3f83ccd007a56b8e64d14
Brayden Tomlinson <humortronic@gmail.com>
2024-02-21 14:12:16 +1030

    Added changes

--- stack_init.c
+++ stack_init.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:02:11 by btomlins          #+#    #+#             */
-/*   Updated: 2024/02/21 14:06:04 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/02/21 14:11:56 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,16 @@ static long ft_atol(const char *s)
 
     result = 0;
     sign = 1;
+    while (*s == ' ' || *s == '\t' || *s == '\n' || \
+            *s == '\r' || *s == '\f' || *s == '\v')
+        s++;
+    if (*s == '-' || *s == '+')
+    {
+        if (*s == '-')
+            sign = -1;
+        s++;
+    }
+    while (ft_isdigit(*s))
+        result = result * 10 + (*s++ - '0');
+    return (result * sign);
 }
