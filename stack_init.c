1893a001eb485897ae8949dcfb15b4b6ac820ce6
Brayden Tomlinson <humortronic@gmail.com>
2024-02-21 14:06:19 +1030

    Added changes

--- stack_init.c
+++ stack_init.c
@@ -6,8 +6,17 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:02:11 by btomlins          #+#    #+#             */
-/*   Updated: 2024/02/21 14:03:02 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/02/21 14:06:04 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static long ft_atol(const char *s)
+{
+    long    result;
+    int     sign;
+
+    result = 0;
+    sign = 1;
+}
