6c20b85e47a5903a00d7d40af27c4df7c76d9c27
Brayden Tomlinson <humortronic@gmail.com>
2024-02-23 16:42:38 +1030

    Added changes

--- stack_utils.c
+++ stack_utils.c
@@ -6,8 +6,21 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 16:36:14 by btomlins          #+#    #+#             */
-/*   Updated: 2024/02/23 16:36:21 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/02/23 16:40:04 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+bool    stack_sorted(t_stack_node   *stack)
+{
+    if (!stack)
+        return (1);
+    while (stack->next)
+    {
+        if (stack->nbr > stack->next->nbr)
+            return (false);
+        stack = stack->next;
+    }
+    return (true);
+}
