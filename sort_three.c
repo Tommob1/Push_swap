bbdd06d5e6a3c8b5b5bb503d8c3d58ceedce2c62
Brayden Tomlinson <humortronic@gmail.com>
2024-03-08 16:17:17 +1030

    Added changes

--- sort_three.c
+++ sort_three.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 16:09:50 by btomlins          #+#    #+#             */
-/*   Updated: 2024/03/08 16:12:59 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/03/08 16:16:59 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 void    sort_three(t_stack_node **a)
 {
-    
+    t_stack_node    *biggest_node;
+
+    biggest_node = find_max(*a);
+    if (biggest_node == *a)
+        ra(a, false);
+    else if ((*a)->next == biggest_node)
+        rra(a, false);
+    if ((*a)->nbr > (*a)->next->nbr)
+        sa(a, false);
 }
