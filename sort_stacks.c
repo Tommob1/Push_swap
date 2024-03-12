dd62af0cd1b1c66b58458baa829a2360a54df3ea
Brayden Tomlinson <humortronic@gmail.com>
2024-03-12 18:06:07 +1030

    Added changes

--- sort_stacks.c
+++ sort_stacks.c
@@ -6,9 +6,32 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 17:59:46 by btomlins          #+#    #+#             */
-/*   Updated: 2024/03/12 18:00:05 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/03/12 18:05:42 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void    sort_stacks(t_stack_node **a, t_stack_node **b)
+{
+    int len_a;
+
+    len_a = stack_len(*a);
+    if (len_a-- > 3 && !stack_sorted(*a))
+        pb(b, a, false);
+    if (len_a-- > 3 && !stack_sorted(*a))
+        pb(b, a, false);
+    while (len_a-- > 3 && !stack_sorted(*a))
+    {
+        init_nodes_b(*a, *b);
+        move_a_to_b(a, b);
+    }
+    sort_three(a);
+    while (*b)
+    {
+        init_nodes_b(*a, *b);
+        move_b_to_a(a, b);
+    }
+    current_index(*a);
+    min_on_top(a);
+}
