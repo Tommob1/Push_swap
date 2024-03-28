0cf638eb57ac5d8cb91eff5a89bce66f71f5932f
Brayden Tomlinson <humortronic@gmail.com>
2024-03-28 17:23:13 +1030

    Added changes to code

--- sort_stacks.c
+++ sort_stacks.c
@@ -6,11 +6,25 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 17:59:46 by btomlins          #+#    #+#             */
-/*   Updated: 2024/03/12 18:05:42 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/03/28 17:22:59 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void move_a_to_b(t_stack_node **a, t_stack_node **b)
+{
+    t_stack_node    *cheapest_node;
+
+    cheapest_node = get_cheapest(*a);
+    if (cheapest_node->above_median && cheapest_node->target_node->above_median)
+        rotate_both(a, b, cheapest_node);
+    else if(!(cheapest_node->above_median)
+        && !(cheapest_node->target_node->above_median))
+        rev_rotate_both(a, b, cheapest_node);
+    prep_for_push(a, cheapest_node, 'a');
+    prep_for_push(b, cheapest_node->target_node, 'b');
+}
 
 void    sort_stacks(t_stack_node **a, t_stack_node **b)
 {
