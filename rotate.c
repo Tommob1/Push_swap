bdd5fdc787fe9aa1340b9d1b087460d49f627e5c
Brayden Tomlinson <humortronic@gmail.com>
2024-04-04 14:27:40 +1030

    Added changes to code

--- rotate.c
+++ rotate.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 17:50:53 by btomlins          #+#    #+#             */
-/*   Updated: 2024/04/04 14:22:54 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/04/04 14:27:24 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,11 @@ static void rotate(t_stack_node **stack)
 
     if(!*stack || !(*stack)->next)
         return ;
-    last_node = find_last(*stack);
+	last_node = find_last(*stack); 
+	last_node->next = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	last_node->next->prev = last_node;
+	last_node->next->next = NULL;
+  
 }
