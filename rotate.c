bb2fce0decac6def7e6e2d2b7b871225ef193b3f
Brayden Tomlinson <humortronic@gmail.com>
2024-04-04 14:23:12 +1030

    Added changes to code

--- rotate.c
+++ rotate.c
@@ -6,9 +6,17 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 17:50:53 by btomlins          #+#    #+#             */
-/*   Updated: 2024/04/04 14:20:30 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/04/04 14:22:54 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void rotate(t_stack_node **stack)
+{
+    t_stack_node    *last_node;
+
+    if(!*stack || !(*stack)->next)
+        return ;
+    last_node = find_last(*stack);
+}
