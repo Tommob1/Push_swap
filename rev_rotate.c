122bf00a0d2f852c05d080cdc2b3e32662f4b517
Brayden Tomlinson <humortronic@gmail.com>
2024-04-04 17:06:12 +1030

    Added changes to code

--- rev_rotate.c
+++ rev_rotate.c
@@ -6,7 +6,28 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 16:43:01 by btomlins          #+#    #+#             */
-/*   Updated: 2024/04/04 16:43:02 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/04/04 17:04:47 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+static void rev_rotate(t_stack_node **stack)
+{
+    
+}
+
+void    rra(t_stack_node    **a, bool   print)
+{
+
+}
+
+void    rrb(t_stack_node    **b, bool   print)
+{
+
+}
+
+void    rrr(t_stack_node    **a, t_stack_node   **b, bool   print)
+{
+    
+}
