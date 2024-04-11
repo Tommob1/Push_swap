31d3c1e7be22a5fda72b897c484a8e47418425b9
Brayden Tomlinson <humortronic@gmail.com>
2024-04-11 15:29:45 +0930

    Added changes to code

---
+++ push.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/11 15:28:11 by btomlins          #+#    #+#             */
+/*   Updated: 2024/04/11 15:29:32 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void push(t_stack_node **dst, t_stack_node **src)
+{
+
+}
+
+void    pa(t_stack_node **a, t_stack_node **b, bool print)
+{
+
+}
+
+void    pb(t_stack_node **b, t_stack_node **a, bool print)
+{
+    
+}
