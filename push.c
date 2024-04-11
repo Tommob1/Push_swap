91badd0b3b755204e3a63f765f3084f01661e5b4
Brayden Tomlinson <humortronic@gmail.com>
2024-04-11 16:06:14 +0930

    Added changes to code

--- push.c
+++ push.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 15:28:11 by btomlins          #+#    #+#             */
-/*   Updated: 2024/04/11 15:29:32 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/04/11 16:05:50 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,26 @@
 
 static void push(t_stack_node **dst, t_stack_node **src)
 {
+    t_stack_node    *push_node;
 
+    if (!*src)
+        return ;
+    push_node = *src;
+    *src = (*src)->next;
+    if (*src)
+        (*src)->prev = NULL;
+    push_node->prev = NULL;
+    if(!*dst)
+    {
+        *dst = push_node;
+        push_node->next = NULL;
+    }
+    else
+    {
+        push_node->next = *dst;
+        push_node->next->prev = push_node;
+        *dst = push_node;
+    }
 }
 
 void    pa(t_stack_node **a, t_stack_node **b, bool print)
