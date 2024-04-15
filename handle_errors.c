874ce157799f9e879f3a11eae8ff006ae162fdd1
Brayden Tomlinson <humortronic@gmail.com>
2024-04-15 16:33:32 +0930

    Added changes to code

---
+++ handle_errors.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_errors.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 16:31:22 by btomlins          #+#    #+#             */
+/*   Updated: 2024/04/15 16:32:56 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int     error_syntax(char *str_n)
+{
+
+}
+
+int     error_duplicate(t_stack_node *a, int n)
+{
+
+}
+
+void    free_stack(t_stack_node **stack)
+{
+
+}
+
+void    free_errors(t_stack_node **a)
+{
+    
+}
