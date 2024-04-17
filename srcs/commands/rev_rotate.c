8945a85a6c40be8508fdb9c5d7d59015b61f9986
Brayden Tomlinson <humortronic@gmail.com>
2024-04-17 14:07:48 +0930

    Added changes to code

---
+++ srcs/commands/rev_rotate.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_rotate.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 16:43:01 by btomlins          #+#    #+#             */
+/*   Updated: 2024/04/04 17:04:47 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void rev_rotate(t_stack_node **stack)
+{
+    
+}
+
+void    rra(t_stack_node    **a, bool   print)
+{
+
+}
+
+void    rrb(t_stack_node    **b, bool   print)
+{
+
+}
+
+void    rrr(t_stack_node    **a, t_stack_node   **b, bool   print)
+{
+    
+}
---
+++ srcs/commands/sort_three.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_three.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/08 16:09:50 by btomlins          #+#    #+#             */
+/*   Updated: 2024/03/08 16:16:59 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void    sort_three(t_stack_node **a)
+{
+    t_stack_node    *biggest_node;
+
+    biggest_node = find_max(*a);
+    if (biggest_node == *a)
+        ra(a, false);
+    else if ((*a)->next == biggest_node)
+        rra(a, false);
+    if ((*a)->nbr > (*a)->next->nbr)
+        sa(a, false);
+}
