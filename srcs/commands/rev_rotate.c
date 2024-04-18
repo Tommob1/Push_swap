8d81a7ebf593c04053667df078e270496568cb16
Brayden Tomlinson <humortronic@gmail.com>
2024-04-18 18:12:54 +0930

    Added changes to code

--- srcs/commands/rev_rotate.c
+++ srcs/commands/rev_rotate.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 16:43:01 by btomlins          #+#    #+#             */
-/*   Updated: 2024/04/04 17:04:47 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/04/18 18:12:29 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,16 @@
 
 static void rev_rotate(t_stack_node **stack)
 {
-    
+    t_stack_node	*last;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	last = find_last(*stack);
+	last->prev->next = NULL;
+	last->next = *stack;
+	last->prev = NULL;
+	*stack = last;
+	last->next->prev = last;
 }
 
 void    rra(t_stack_node    **a, bool   print)
