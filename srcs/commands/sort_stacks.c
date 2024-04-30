d62ed38080bbf454c6efcd2f1131735a6f83359f
Brayden Tomlinson <humortronic@gmail.com>
2024-04-30 14:01:30 +0930

    Added changes to code

--- srcs/commands/sort_stacks.c
+++ srcs/commands/sort_stacks.c
@@ -6,11 +6,33 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 17:59:46 by btomlins          #+#    #+#             */
-/*   Updated: 2024/04/19 16:26:34 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/04/30 14:00:40 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/push_swap.h"
+
+static void rotate_both(t_stack_node **a, t_stack_node **b, 
+                                        t_stack_node *cheapest_node)
+{
+    while (*b != cheapest_node->target_node && *a != cheapest_node)
+    {
+        rr(a, b, false);
+    }
+    current_index(*a);
+    current_index(*b);
+}
+
+static void rev_rotate_both(t_stack_node **a, t_stack_node **b,
+                            t_stack_node *cheapest_node)
+{
+    while (*b != cheapest_node->target_node && *a != cheapest_node)
+    {
+        rrr(a, b, false);
+    }
+    current_index(*a);
+    current_index(*b);
+}
 
 static void move_a_to_b(t_stack_node **a, t_stack_node **b)
 {
