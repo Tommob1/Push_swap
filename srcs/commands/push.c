dab77aa7ecd59675197f6361b63c2414eb104d20
Brayden Tomlinson <humortronic@gmail.com>
2024-04-30 15:54:35 +0930

    Added changes to code

--- srcs/commands/push.c
+++ srcs/commands/push.c
@@ -6,46 +6,46 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 15:28:11 by btomlins          #+#    #+#             */
-/*   Updated: 2024/04/19 16:26:30 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/04/30 15:54:21 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/push_swap.h"
 
-static void push(t_stack_node **dst, t_stack_node **src)
+static void	push(t_stack_node **dst, t_stack_node **src)
 {
-    t_stack_node    *push_node;
+    t_stack_node	*push_node;
 
-    if (!*src)
-        return ;
-    push_node = *src;
-    *src = (*src)->next;
-    if (*src)
-        (*src)->prev = NULL;
-    push_node->prev = NULL;
-    if(!*dst)
-    {
-        *dst = push_node;
-        push_node->next = NULL;
-    }
-    else
-    {
-        push_node->next = *dst;
-        push_node->next->prev = push_node;
-        *dst = push_node;
-    }
+	if (!*src)
+		return ;
+	push_node = *src;
+	*src = (*src)->next;
+	if (*src)
+		(*src)->prev = NULL;
+	push_node->prev = NULL;
+	if(!*dst)
+	{
+		*dst = push_node;
+		push_node->next = NULL;
+	}
+	else
+	{
+		push_node->next = *dst;
+		push_node->next->prev = push_node;
+		*dst = push_node;
+	}
 }
 
-void    pa(t_stack_node **a, t_stack_node **b, bool print)
+void	pa(t_stack_node **a, t_stack_node **b, bool print)
 {
-    push(a, b);
-    if (!print)
-        ft_printf("pa\n");
+	push(a, b);
+	if (!print)
+		ft_printf("pa\n");
 }
 
-void    pb(t_stack_node **b, t_stack_node **a, bool print)
+void	pb(t_stack_node **b, t_stack_node **a, bool print)
 {
-    push(b, a);
-    if (!print)
-        ft_printf("pb\n");
+	push(b, a);
+	if (!print)
+		ft_printf("pb\n");
 }
