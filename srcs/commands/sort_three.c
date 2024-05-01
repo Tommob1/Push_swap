340e6a9b4da31602b2f0d2d24bd8e81ff5475146
Brayden Tomlinson <humortronic@gmail.com>
2024-05-01 17:02:23 +0930

    Added changes to code

--- srcs/commands/sort_three.c
+++ srcs/commands/sort_three.c
@@ -6,21 +6,21 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 16:09:50 by btomlins          #+#    #+#             */
-/*   Updated: 2024/04/19 16:26:35 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/05/01 17:02:05 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/push_swap.h"
 
-void    sort_three(t_stack_node **a)
+void	sort_three(t_stack_node **a)
 {
-    t_stack_node    *biggest_node;
+	t_stack_node	*biggest_node;
 
-    biggest_node = find_max(*a);
-    if (biggest_node == *a)
-        ra(a, false);
-    else if ((*a)->next == biggest_node)
-        rra(a, false);
-    if ((*a)->nbr > (*a)->next->nbr)
-        sa(a, false);
+	biggest_node = find_max(*a);
+	if (biggest_node == *a)
+		ra(a, false);
+	else if ((*a)->next == biggest_node)
+		rra(a, false);
+	if ((*a)->nbr > (*a)->next->nbr)
+		sa(a, false);
 }
