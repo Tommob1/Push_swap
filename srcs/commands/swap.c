cf3431b3bac20a28a6dafac9fcb613d6aa158e5d
Brayden Tomlinson <humortronic@gmail.com>
2024-05-01 17:03:54 +0930

    Added changes to code

--- srcs/commands/swap.c
+++ srcs/commands/swap.c
@@ -6,43 +6,43 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 16:24:08 by btomlins          #+#    #+#             */
-/*   Updated: 2024/04/19 16:26:36 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/05/01 17:03:39 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/push_swap.h"
 
-static void swap(t_stack_node **head)
+static void	swap(t_stack_node **head)
 {
-    if (!*head || !(*head)->next)
-        return ;
-    *head = (*head)->next;
-    (*head)->prev->prev = *head;
-    (*head)->prev->next = (*head)->next;
-    if ((*head)->next)
-        (*head)->next->prev = (*head)->prev;
-    (*head)->next = (*head)->prev;
-    (*head)->prev = NULL;
+	if (!*head || !(*head)->next)
+		return ;
+	*head = (*head)->next;
+	(*head)->prev->prev = *head;
+	(*head)->prev->next = (*head)->next;
+	if ((*head)->next)
+		(*head)->next->prev = (*head)->prev;
+	(*head)->next = (*head)->prev;
+	(*head)->prev = NULL;
 }
 
-void    sa(t_stack_node **a, bool print)
+void	sa(t_stack_node **a, bool print)
 {
-    swap(a);
-    if (!print)
-        ft_printf("sa\n");
+	swap(a);
+	if (!print)
+		ft_printf("sa\n");
 }
 
-void    sb(t_stack_node **b, bool print)
+void	sb(t_stack_node **b, bool print)
 {
-    swap(b);
-    if (!print)
-        ft_printf("sb\n");
+	swap(b);
+	if (!print)
+		ft_printf("sb\n");
 }
 
-void    ss(t_stack_node **a, t_stack_node **b, bool print)
+void	ss(t_stack_node **a, t_stack_node **b, bool print)
 {
-    swap(a);
-    swap(b);
-    if (!print)
-        ft_printf("ss\n");
+	swap(a);
+	swap(b);
+	if (!print)
+		ft_printf("ss\n");
 }
