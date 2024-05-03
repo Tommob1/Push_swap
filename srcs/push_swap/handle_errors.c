405cc8495360969a6aa78b4ea65d5dc0950ce848
Brayden Tomlinson <humortronic@gmail.com>
2024-05-03 15:55:12 +0930

    Added changes to code

--- srcs/push_swap/handle_errors.c
+++ srcs/push_swap/handle_errors.c
@@ -6,60 +6,61 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 16:31:22 by btomlins          #+#    #+#             */
-/*   Updated: 2024/04/19 16:26:38 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/05/03 15:54:41 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/push_swap.h"
 
-int     error_syntax(char *str_n)
+int	error_syntax(char *str_n)
 {
-    if (!(*str_n == '+' || *str_n == '-' || (*str_n >= '0' && *str_n <= '9')))
-        return(1);
-    if((*str_n == '+' || *str_n == '-') && !(str_n[1] >= '0' && str_n[1] <= '9'))
-        return(1);
-    while (*++str_n)
-    {
-        if (!(*str_n >= '0' && *str_n <= '9'))
-            return(1);
-    }
-    return(0);
+	if (!(*str_n == '+' || *str_n == '-' || (*str_n >= '0' && *str_n <= '9')))
+		return (1);
+	if ((*str_n == '+' || *str_n == '-')
+		&& !(str_n[1] >= '0' && str_n[1] <= '9'))
+		return (1);
+	while (*++str_n)
+	{
+		if (!(*str_n >= '0' && *str_n <= '9'))
+			return (1);
+	}
+	return (0);
 }
 
-int     error_duplicate(t_stack_node *a, int n)
+int	error_duplicate(t_stack_node *a, int n)
 {
-    if (!a)
-        return(0);
-    while (a)
-    {
-        if (a->nbr == n)
-            return(1);
-        a = a->next;
-    }
-    return(0);
+	if (!a)
+		return (0);
+	while (a)
+	{
+		if (a->nbr == n)
+			return (1);
+		a = a->next;
+	}
+	return (0);
 }
 
-void    free_stack(t_stack_node **stack)
+void	free_stack(t_stack_node **stack)
 {
-    t_stack_node    *tmp;
-    t_stack_node    *current;
+	t_stack_node	*tmp;
+	t_stack_node	*current;
 
-    if (!stack)
-        return ;
-    current = *stack;
-    while (current)
-    {
-        tmp = current->next;
-        current->nbr = 0;
-        free(current);
-        current = tmp;
-    }
-    *stack = NULL;
+	if (!stack)
+		return ;
+	current = *stack;
+	while (current)
+	{
+		tmp = current->next;
+		current->nbr = 0;
+		free(current);
+		current = tmp;
+	}
+	*stack = NULL;
 }
 
-void    free_errors(t_stack_node **a)
+void	free_errors(t_stack_node **a)
 {
-    free_stack(a);
-    ft_printf("Error\n");
-    exit(1);
+	free_stack(a);
+	ft_printf("Error\n");
+	exit(1);
 }
